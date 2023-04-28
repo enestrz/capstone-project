f57fede95a2667661b3088a84eb0f04c64bf30b6
Enes Terzi <enestrz15@gmail.com>
2023-04-28 22:46:14 +0300

    Semantic and meta tags added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import Navigation from "./component/Navigation";
+
 function App() {
-    return <div>Homepage</div>;
+    return (
+        <>
+            <Header />
+            <Navigation />
+            <Main />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/component/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav>
+            <img src="../../public/images/Logo.svg" alt="Little Lemon logo" />
+            <ul>
+                <li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
